375c005901a3e794a6f0a49c51bd360001fa7757
yjs <wotlr789@naver.com>
2020-12-29 00:24:24 +0900

    Querydsl 적용 예제

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -63,7 +63,8 @@ public class OrderService {
      * 주문 검색
      */
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAll(orderSearch);
+        List<Order> orders = orderRepository.findAllQuerydsl(orderSearch);
+        return orders;
     }
 
 }
